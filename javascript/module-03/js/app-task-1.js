ee217c758db3f6df11b81478fecd1fd697d4937b
Eugene Kolomiets <kolomiets1990@gmail.com>
2019-03-28 15:37:52 +0200

    JS Module 3

---
+++ javascript/module-03/js/app-task-1.js
@@ -0,0 +1,27 @@
+const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
+
+const isLoginValid = function(login) {
+  return login.length >= 4 && login.length <= 16;
+};
+
+const isLoginUnique = function(allLogins, login) {
+  return !allLogins.includes(login);
+};
+
+//Но я бы сделал 1 return а в let msg вставлял значения и в конце вернул бы значение msg... или так не естетично?
+const addLogin = function(allLogins, login) {
+  if (!isLoginValid(login)) {
+    return "Ошибка! Логин должен быть от 4 до 16 символов";
+  } else if (!isLoginUnique(allLogins, login)) {
+    return "Такой логин уже используется!";
+  } else {
+    allLogins.push(login);
+    return "Логин успешно добавлен!";
+  }
+};
+
+// Вызовы функции для проверки
+console.log(addLogin(logins, "Ajax")); // 'Логин успешно добавлен!'
+console.log(addLogin(logins, "robotGoogles")); // 'Такой логин уже используется!'
+console.log(addLogin(logins, "Zod")); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+console.log(addLogin(logins, "jqueryisextremelyfast")); // 'Ошибка! Логин должен быть от 4 до 16 символов'
